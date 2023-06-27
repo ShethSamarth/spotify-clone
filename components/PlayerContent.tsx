2f0610666c21d0e7f5987dfe30858487c4a619d2
Samarth Sheth <samarth.sheth.29@gmail.com>
2023-06-27 23:05:34 +0530

    auth bug fix

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -100,7 +100,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
       <div className="flex md:hidden col-auto w-full justify-end items-center">
         <div
           onClick={handlePlay}
-          className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
+          className="h-10 w-10 mr-3 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
         >
           <Icon size={30} className="text-black" />
         </div>
